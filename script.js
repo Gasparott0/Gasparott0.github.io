c6bce546c4c5b90fd45bbac1fdd74aeb4ef02340
Gasparott0 <mtsgasparotto@hotmail.com>
2022-08-27 20:10:54 -0300

    creation of skills section and changes on aboutme section

---
+++ script.js
@@ -0,0 +1,7 @@
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal('#introduction', { delay: 150 });
+ScrollReveal().reveal('#about-me');
+ScrollReveal().reveal('#skills');
+
+ScrollReveal().reveal('article', { delay: 300, interval: 350 });
